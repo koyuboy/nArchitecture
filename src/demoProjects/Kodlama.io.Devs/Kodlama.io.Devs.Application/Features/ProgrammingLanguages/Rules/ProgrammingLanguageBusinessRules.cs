d7c1e4b4a5e0b94582f87d42bea90e4645c2b59b
Ahmet Huzeyfe Demir <35155252+koyuboy@users.noreply.github.com>
2022-09-11 13:39:06 +0300

    Created base structer for the Kodlama.io.Devs project & created DB

---
+++ src/demoProjects/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,33 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming Language name exists.");
+        }
+
+        public void ProgrammingLanguageShouldExistWhenRequested(ProgrammingLanguage programmingLanguage)
+        {
+            if (programmingLanguage == null) throw new BusinessException("Requested programming language does not exists.");
+        }
+    }
+}
---
+++ src/demoProjects/Kodlama.io.Devs/Kodlama.io.Devs.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Persistence.Contexts;
+using Kodlama.io.Devs.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("Kodlama.io.DevsConnectionString")));
+
+            services.AddScoped<IProgrammingLanguageRepository, ProgrammingLanguageRepository>();
+
+            return services;
+        }
+    }
+}
